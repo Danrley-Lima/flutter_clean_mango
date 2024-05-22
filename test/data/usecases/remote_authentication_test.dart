aace1877e2d01d0d3b47597f82184d561e2fcffe
Danrley Lima <danrleylac@gmail.com>
2024-05-22 20:21:14 -0300

    test: ensure RemoteAuthentication calls HttpClient with correct Body

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,10 +1,4 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:ForDev/domain/usecases/authetication.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meta/meta.dart';
@@ -16,13 +10,21 @@ class RemoteAuthentication {
 
   RemoteAuthentication({@required this.httpClient, @required this.url});
 
-  Future<void> auth() async {
-    await httpClient.request(url: url, method: "post");
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {
+      "email": params.email,
+      "password": params.password,
+    };
+    await httpClient.request(url: url, method: "post", body: body);
   }
 }
 
 abstract class HttpClient {
-  Future<void> request({@required String url, @required String method});
+  Future<void> request({
+    @required String url,
+    @required String method,
+    Map body,
+  });
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
@@ -39,13 +41,19 @@ void main() {
   });
 
   test("Should call HttpClient with correct values", () async {
-    await sut.auth();
+    final params = AuthenticationParams(
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    );
 
-    await sut.auth();
+    await sut.auth(params);
 
-    verify(httpClient.request(
-      url: url,
-      method: 'post',
-    ));
+    verify(
+      httpClient.request(
+        url: url,
+        method: 'post',
+        body: {"email": params.email, "password": params.password},
+      ),
+    );
   });
 }
