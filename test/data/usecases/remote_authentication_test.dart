1971d5a46a8380a525513bcd70bb0ee9136275a3
Danrley Lima <danrleylac@gmail.com>
2024-05-22 21:17:27 -0300

    test: ensure RemoteAuthentication throws Unexpected on 404

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -52,4 +52,28 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test("Should throw UnexpectedError if HttpClient return 404", () async {
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenThrow(HttpError.notFound);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test("Should throw UnexpectedError if HttpClient return 400", () async {
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenThrow(HttpError.notFound);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
